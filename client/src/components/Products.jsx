8c1cb652866bf110155f59a330e39e42192a4bae
R2H <r2h@AdminisatorsMBP.attlocal.net>
2023-01-07 21:52:36 -0500

    update

--- client/src/components/Products.jsx
+++ client/src/components/Products.jsx
@@ -15,7 +15,7 @@ const Products = () => {
     useEffect(()=>{
         const getAllProducts = async()=>{
             try{
-                const res = await axios.get('http://localhost:5000/products')
+                const res = await axios.get('http://18.118.4.172:5000/products')
                 setProduct(res.data);
                 setFiltered(res.data);
             }catch (err){
